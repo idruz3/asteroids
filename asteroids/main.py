02183186fe0706870fcffef1c5b7f56a19641b07
idruz <idrusramadhan24@gmail.com>
2025-08-02 15:34:21 +0700

    asteroids

--- asteroids/main.py
+++ asteroids/main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -9,15 +11,21 @@ def main():
     clock = pygame.time.Clock()
     dt = 0 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
+   
     
     updateable = pygame.sprite.Group()
     drawable= pygame.sprite.Group()
-    Player.containers = updateable, drawable
-    player = Player(x,y)
+    asteroids = pygame.sprite.Group()
     
+    Player.containers = updateable, drawable
 
+    
+    Asteroid.containers = asteroids, updateable, drawable
+    AsteroidField.containers = (updateable,)
+    asteroid_field = AsteroidField()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    
     
     while True:
         
